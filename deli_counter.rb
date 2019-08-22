fa03b15b04ff971e063207cdece20bee00c9b543
Becky Zorn <bczorn@gmail.com>
2019-08-22 11:35:48 +0000

    Automatically backed up by Learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -1,1 +1,16 @@
 # Write your code here.
+def line(deli)
+  if deli == nil
+    "The line is currently empty"
+  else 
+    x = deli.size
+    "There are currently #{x} people in line"
+end
+
+def take_a_number
+  
+end
+
+def now_serving
+  
+end
