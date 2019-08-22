ecf6b82581422481aba8160a0dd45ce49b78ead7
Becky Zorn <bczorn@gmail.com>
2019-08-22 11:49:19 +0000

    Automatically backed up by Learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -4,7 +4,7 @@ def line(deli)
     puts "The line is currently empty."
   else 
     deli.each do |x|
-    c = "The line is currently:"
+    c = ["The line is currently:"]
     y = deli.index(x) + 1
     c.push(" #{y}. #{x}")
     puts c
