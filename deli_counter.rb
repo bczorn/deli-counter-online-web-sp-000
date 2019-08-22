3f1d9b3f6bced8128778feb66f71f304df8c8027
Becky Zorn <bczorn@gmail.com>
2019-08-22 11:37:44 +0000

    Automatically backed up by Learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -1,10 +1,10 @@
 # Write your code here.
 def line(deli)
   if deli.size == 0
-    puts "The line is currently empty"
+    puts "The line is currently empty."
   else 
     x = deli.size
-    puts "There are currently #{x} people in line"
+    puts "There are currently #{x} people in line."
   end
 end
 
